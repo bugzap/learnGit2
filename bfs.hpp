7b6087fcd07d884e3d831e25be324393a9b321b9
bugzap <raghavpaul90@gmail.com>
2022-08-02 22:53:24 -0400

    added bfs function

--- bfs.hpp
+++ bfs.hpp
@@ -1,8 +1,30 @@
 #pragma once
+#include <iostream>
 
 namespace graph
 {
-    class BFS{
+
+    struct Node{
+        Node* left;
+        Node* right;
+        int value;
+    }
+    void BFS(Node* aRoot){
         
     };
+
+    void DFS (Node* aRoot){
+        if (aRoot == nullptr){
+            return;
+        }
+        
+        DFS(aRoot->left);
+        
+        std::cout << aRoot->value;
+
+        DFS(aRoot->right);        
+
+    }
+
+
 }
